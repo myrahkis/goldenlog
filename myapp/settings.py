c4b73661789487bdb2ad7d5892089aab324944e6
Mira <kruzinka@yandex.ru>
2022-12-13 21:36:29 +0300

    migration

--- myapp/settings.py
+++ myapp/settings.py
@@ -17,7 +17,6 @@ from django.urls import reverse_lazy
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
