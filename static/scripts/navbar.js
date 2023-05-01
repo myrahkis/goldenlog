54389f6c61dedadf52f60b13cfa48afc8b60a8eb
Mira <kruzinka@yandex.ru>
2023-05-01 19:11:47 +0300

    little navbar anim + removed redundant styles

---
+++ static/scripts/navbar.js
@@ -0,0 +1,15 @@
+// $('.nav__toggle-btn').click(function () {
+//     $(this).toggleClass('active');
+//     console.log("Clicked menu button");
+//     $("#mainListDiv").toggleClass("nav__wrapper--visible");
+//     $("#mainListDiv").fadeIn();
+//
+// });
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 320) {
+        $('.navbar').addClass('highlight');
+    } else {
+        $('.navbar').removeClass('highlight');
+    }
+});
